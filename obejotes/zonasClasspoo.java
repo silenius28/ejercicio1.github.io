9f05059487b059523f3febcf107255f440d712fe
silenius28 <133064221+silenius28@users.noreply.github.com>
2023-05-15 20:22:04 -0500

    Add files via upload

---
+++ obejotes/zonasClasspoo.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class zonasClasspoo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class zonasClasspoo
+{
+   int afectacion;
+   String ciudad;
+   int cont = 0;
+   int cont1 = 0;
+   String nombre;
+   int sal_mar;
+}
